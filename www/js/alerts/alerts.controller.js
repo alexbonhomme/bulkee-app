434de8c10d1a3f7373e5c2bb9d19f1a095c5abee
Benjamin Coenen <benjamin.coenen@hotmail.com>
2015-10-10 22:20:14 +0200

    feat (alerts): add ws

--- www/js/alerts/alerts.controller.js
+++ www/js/alerts/alerts.controller.js
@@ -5,15 +5,83 @@
         .module('bulkee.alerts')
         .controller('AlertsController', AlertsController);
 
-    AlertsController.$inject = [];
+    AlertsController.$inject = ['Alerts', 'Map', '$cordovaGeolocation', '$state'];
 
-    function AlertsController() {
+    function AlertsController(Alerts, Map, $cordovaGeolocation, $state) {
         var vm = this;
+        var category = {};
+        vm.distance = 50;
+        var posOptions = {
+          enableHighAccuracy: false,
+          timeout: 10000
+        };
+        vm.category = {
+          FURNITURES: false,
+          DECO: false,
+          CULTURE: false,
+          ELECTRO: false,
+          MULTIMEDIA: false,
+          LITTLEELECTRO: false,
+          CLOTHES: false,
+          TOYS: false,
+          MISC: false
+        };
 
         init();
 
         function init() {
+          $cordovaGeolocation.getCurrentPosition(posOptions)
+            .then(function (position) {
+              vm.position = [position.coords.longitude, position.coords.latitude];
+              return Map.getAddress(vm.position[0], vm.position[1]);
+            })
+            .then(function (address) {
+              vm.address = address;
+            });
 
         }
+
+        vm.createAlert = function () {
+          console.log('position : ', vm.position);
+          var data = {
+            categories: getCategories(),
+            distance: vm.distance,
+            position: vm.position
+          };
+
+          console.log('category : ', data);
+
+          Alerts.postAlert(data)
+            .then(function () {
+              $state.go('home');
+              window.plugins.toast.showWithOptions(
+                {
+                  message: 'Changement effectué avec succès',
+                  duration: 'short',
+                  position: 'bottom',
+                  addPixelsY: -40  // added a negative value to move it up a bit (default 0)
+                }
+              );
+            })
+        };
+
+        function getCategories() {
+          var index = 0;
+          var tab = [];
+          _.forEach(vm.category, function (elt, key) {
+            if (elt) {
+              tab.push({
+                id: index++,
+                name: key
+              });
+            } else {
+              return null;
+            }
+          });
+
+          return tab;
+        }
+
+        init();
     }
 })();
