674095c8e3cdbb69e1de74af61d6d379ee7eb5f2
Benjamin Coenen <benjamin.coenen@hotmail.com>
2015-10-10 23:54:47 +0200

    feat (notifs): add user ids

--- www/js/app.js
+++ www/js/app.js
@@ -60,6 +60,18 @@
               alert('Device token: ' + token.token);
               $rootScope.devicetoken = token.token;
             });
+
+            // this will give you a fresh user or the previously saved 'current user'
+            var user = Ionic.User.current();
+
+            // if the user doesn't have an id, you'll need to give it one.
+            if (!user.id) {
+              user.id = Ionic.User.anonymousId();
+              // user.id = 'your-custom-user-id';
+            }
+
+            //persist the user
+            user.save();
         });
     }
 
