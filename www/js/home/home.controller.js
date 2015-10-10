acdcd01a22e8f1c6e9187da8e0a0fc2759725112
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2015-10-10 11:11:22 +0200

    Adds home module structure as model

---
+++ www/js/home/home.controller.js
@@ -0,0 +1,19 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('bulkee.home')
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = [];
+
+    function HomeController() {
+        var vm = this;
+
+        init();
+
+        function init() {
+
+        }
+    }
+})();
